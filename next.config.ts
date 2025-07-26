6bdd4dfc5d1bd40b6b9277b1b62fbc05498ca826
Hector <baseballhector@gmail.com>
2025-07-26 15:39:16 -0400

    config: add dotenv for environment variable management and import IP address from env file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+import dotenv from "dotenv";
+import path from "path";
+
+// Manually load .env.local
+dotenv.config({ path: path.resolve(__dirname, ".env.local") });
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    process.env.DEV_ALLOWED_ORIGIN || "http://localhost:3000",
+  ],
 };
 
 export default nextConfig;
