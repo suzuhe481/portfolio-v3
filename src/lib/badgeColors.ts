b187ee5220708400b809d1b22d36b102c1805695
Hector <baseballhector@gmail.com>
2025-08-06 21:39:20 -0400

    style: Adding styles for badge color variants

---
+++ src/lib/badgeColors.ts
@@ -0,0 +1,23 @@
+export const badgeColorVariants: Record<string, string> = {
+  yellow:
+    "inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-sm md:text-base font-medium text-yellow-800 ring-1 ring-yellow-600/20 ring-inset",
+  blue: "inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-sm md:text-base font-medium text-blue-700 ring-1 ring-blue-700/10 ring-inset",
+  cyan: "inline-flex items-center rounded-md bg-cyan-50 px-2 py-1 text-sm md:text-base font-medium text-cyan-700 ring-1 ring-cyan-700/10 ring-inset",
+  green:
+    "inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-sm md:text-base font-medium text-green-700 ring-1 ring-green-600/20 ring-inset",
+  indigo:
+    "inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-sm font-medium text-indigo-700 ring-1 ring-indigo-700/10 ring-inset",
+  purple:
+    "inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-sm md:text-base font-medium text-purple-700 ring-1 ring-purple-700/10 ring-inset",
+  gray: "inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-sm md:text-base font-medium text-gray-600 ring-1 ring-gray-500/10 ring-inset",
+  orange:
+    "inline-flex items-center rounded-md bg-orange-50 px-2 py-1 text-sm md:text-base font-medium text-orange-700 ring-1 ring-orange-600/20 ring-inset",
+  red: "inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-sm md:text-base font-medium text-red-700 ring-1 ring-red-600/20 ring-inset",
+  pink: "inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-sm md:text-base font-medium text-pink-700 ring-1 ring-pink-600/20 ring-inset",
+  black:
+    "inline-flex items-center rounded-md bg-gray-900 px-2 py-1 text-sm md:text-base font-medium text-white ring-1 ring-gray-800 ring-inset",
+  white:
+    "inline-flex items-center rounded-md bg-white px-2 py-1 text-sm md:text-base font-medium text-gray-900 ring-1 ring-gray-200 ring-inset",
+  "dark-green":
+    "inline-flex items-center rounded-md bg-green-800 px-2 py-1 text-sm md:text-base font-medium text-white ring-1 ring-green-900/30 ring-inset",
+};
