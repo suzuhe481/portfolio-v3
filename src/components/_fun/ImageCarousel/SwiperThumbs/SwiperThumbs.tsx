d6627ede2047ac16552c26e61a24fde26c413027
Hector <baseballhector@gmail.com>
2025-09-11 18:04:14 -0400

    feat: swiper gallery to display thumbnail images

---
+++ src/components/_fun/ImageCarousel/SwiperThumbs/SwiperThumbs.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Dispatch, SetStateAction } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import type { Swiper as SwiperClass } from "swiper";
+
+import Image from "next/image";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+interface ISwiperThumbsProps {
+  imagesData: string[];
+  activeIndex: number;
+  setActiveIndex: Dispatch<SetStateAction<number>>;
+  setThumbsSwiper: (swiper: SwiperClass) => void;
+  mainSwiper: SwiperClass | null;
+}
+
+const SwiperThumbs = ({
+  imagesData,
+  setThumbsSwiper,
+  activeIndex,
+  mainSwiper,
+}: ISwiperThumbsProps) => {
+  return (
+    <Swiper
+      onSwiper={setThumbsSwiper}
+      spaceBetween={10}
+      slidesPerView={4}
+      freeMode={true}
+      watchSlidesProgress={true}
+      modules={[FreeMode, Navigation, Thumbs]}
+      className="mySwiper"
+    >
+      {imagesData.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className="flex w-full justify-center">
+            <div
+              onClick={() => mainSwiper?.slideToLoop(index)}
+              className={`relative w-full h-[100px] ${
+                activeIndex === index ? "" : "brightness-50"
+              }`}
+            >
+              <Image
+                src={image}
+                alt=""
+                className="cursor-pointer"
+                fill
+                loading="lazy"
+              />
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default SwiperThumbs;
