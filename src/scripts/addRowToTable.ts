de3da618cbbe79e26f2c694852dabe2f535bbdde
Hector <baseballhector@gmail.com>
2025-09-16 20:24:02 -0400

    fix: changing how env file is loaded into script

--- src/scripts/addRowToTable.ts
+++ src/scripts/addRowToTable.ts
@@ -1,4 +1,10 @@
+import path from "path";
+import dotenv from "dotenv";
+
 import createSupabaseClient from "./server/createSupabaseClient";
+
+// Manually load .env.local from project root, not script root
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
 
 /**
  * Adds a single row to the given Supabase table.
@@ -33,12 +39,19 @@ async function addRowToTable(
 }
 
 // Data to pass into the function
-const table_name = "travel_locations";
+// const table_name = "travel_locations";
+// const rowData = {
+//   location: "Osaka",
+//   country: "Japan",
+//   latitude: 34.6937,
+//   longitude: 135.5023,
+// };
+
+const table_name = "builds";
 const rowData = {
-  location: "Osaka",
-  country: "Japan",
-  latitude: 34.6937,
-  longitude: 135.5023,
+  name: "PS5 DualSense Mod",
+  main_description:
+    "A modification to my PlayStation 5 DualSense controller where I change the faceplate, add rear buttons, and swap the joysticks to TMR joysticks.",
 };
 
 addRowToTable(table_name, rowData).catch((err) => console.error(err));
