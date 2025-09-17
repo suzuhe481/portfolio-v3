c186f669f131c15146353c7002033df299518b5f
Hector <baseballhector@gmail.com>
2025-09-16 20:31:24 -0400

    fix: adjust script to use new supabase key

--- src/scripts/server/createSupabaseClient.ts
+++ src/scripts/server/createSupabaseClient.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
  * Creates a Supabase client instance.
  *
  * Throws an error if the environment variables
- * `SUPABASE_PUBLIC_URL` and `SUPABASE_PUBLISHABLE_DEFAULT_KEY`
+ * `SUPABASE_PUBLIC_URL` and `SUPABASE_SECRET_SERVICE_ROLE_KEY`
  * are not set.
  *
  * @throws {Error} If the environment variables are not set.
@@ -14,7 +14,7 @@ import { createClient } from "@supabase/supabase-js";
 export default async function createSupabaseClient() {
   if (
     !process.env.SUPABASE_PUBLIC_URL ||
-    !process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY
+    !process.env.SUPABASE_SECRET_SERVICE_ROLE_KEY
   ) {
     console.error("Supabase credentials not found");
     throw new Error("Supabase environment variables are not set.");
@@ -24,7 +24,7 @@ export default async function createSupabaseClient() {
     // Supabase Client
     const supabase = createClient(
       process.env.SUPABASE_PUBLIC_URL!,
-      process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY!
+      process.env.SUPABASE_SECRET_SERVICE_ROLE_KEY!
     );
 
     return supabase;
