f5b5b947a551d5a873ca5ad2133e498227a7e9bb
Hector <baseballhector@gmail.com>
2025-09-17 19:36:58 -0400

    feat: implementing dndkit context to drag card components

--- src/components/_main/TechStack/TechStack.tsx
+++ src/components/_main/TechStack/TechStack.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FC, useId, useState, useCallback } from "react";
 import {
   TypeScriptIcon,
   ReactIcon,
@@ -19,6 +20,26 @@ import {
 import { Card } from "./Card/Card";
 import { CardContainer } from "./CardContainer/CardContainer";
 import { FixedBackground } from "../FixedBackground/FixedBackground";
+
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+  DragStartEvent,
+  DragEndEvent,
+  DragOverlay,
+  closestCorners,
+} from "@dnd-kit/core";
+
+import {
+  arrayMove,
+  sortableKeyboardCoordinates,
+  SortableContext,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
 
 import { ITechStackDataProps } from "@/types";
 
@@ -81,7 +102,69 @@ const techStackData: ITechStackDataProps[] = [
   },
 ];
 
+const DefaultIcon: FC = () => {
+  return <span>Icon not found</span>;
+};
+
 export const TechStack = () => {
+  const [techStack, setTechStack] = useState(techStackData);
+  const [draggedTech, setDraggedTech] = useState<string | null>(null); // Stores the name of the dragged tech card.
+
+  // Sensors for dndkit
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5,
+      },
+    }),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const dndId = useId(); // Fixes hydration issues when assigned to DndContext
+
+  // Find tech icon by name.
+  // If not found, return default icon.
+  const findTechIconByName = useCallback((name: string) => {
+    const tech = techStackData.find((tech) => tech.name === name);
+    const icon = tech?.icon;
+
+    if (icon) {
+      return icon;
+    }
+
+    return DefaultIcon;
+  }, []);
+
+  // Dndkit handler
+  // Sets the dragged tech card.
+  const handleDragStart = useCallback((event: DragStartEvent) => {
+    const { active } = event;
+
+    const { id } = active;
+
+    setDraggedTech(id as string);
+  }, []);
+
+  // Dndkit handler
+  // Reorders the tech stack when drag ends.
+  const handleDragEnd = useCallback((event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setTechStack((items) => {
+        const oldIndex = items.findIndex((item) => item.name === active.id);
+        const newIndex = items.findIndex((item) => item.name === over?.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+
+      setDraggedTech(null);
+    }
+  }, []);
+
   return (
     <div id="tech" className="relative py-12 min-h-screen">
       {/* Background SVG Image */}
@@ -90,12 +173,40 @@ export const TechStack = () => {
       <h1 className="flex flex-col items-center text-white text-6xl pt-12 underline font-plagiata">
         Tech Stack
       </h1>
+      <p className="flex flex-col items-center text-white text-mde font-plagiata">
+        Hover/Tap/Drag The Cards!
+      </p>
       <div className="flex justify-center items-center">
-        {/* Tech Icons*/}
         <CardContainer className="font-plagiata font-bold">
-          {techStackData.map((tech) => (
-            <Card key={tech.name} name={tech.name} icon={tech.icon} />
-          ))}
+          <DndContext
+            id={dndId}
+            sensors={sensors}
+            collisionDetection={closestCorners}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext
+              items={techStack.map((tech) => tech.name)}
+              strategy={rectSortingStrategy}
+            >
+              {/* Tech Icons*/}
+              {techStack.map((tech) => (
+                <Card key={tech.name} name={tech.name} icon={tech.icon} />
+              ))}
+            </SortableContext>
+
+            {/* The cloned placeholder of the dragged card that remains in the grid. */}
+            <DragOverlay>
+              {draggedTech ? (
+                <Card
+                  key={draggedTech}
+                  name={draggedTech}
+                  icon={findTechIconByName(draggedTech)}
+                  overlay={true}
+                />
+              ) : null}
+            </DragOverlay>
+          </DndContext>
         </CardContainer>
       </div>
 
